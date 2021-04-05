fd021b9a9503198d895e88f4e24acceba046d82b
Mauricio Pérez <mperez.lagos@gmail.com>
2021-04-04 23:23:01 -0400

    Agregada Validaciones al ingreso de Usuarios

---
+++ database/config.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+
+    try {
+
+        await mongoose.connect(process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
+        console.log('Base de Datos Online');
+
+
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a  la hora de Iniciar la base de datos');
+    }
+
+
+};
+
+
+
+module.exports = {
+    dbConnection
+};
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,27 +1,35 @@
-
 const { Router } = require('express');
+const { check } = require('express-validator');
 
-const { usuariosGet,
-        usuariosPut,
-        usuariosPost,
-        usuariosDelete,
-        usuariosPatch } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido, emailExiste } = require('../helpers/db-validators');
+
+const {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+} = require('../controllers/usuarios');
 
 const router = Router();
 
 
-router.get('/', usuariosGet );
+router.get('/', usuariosGet);
 
-router.put('/:id', usuariosPut );
+router.put('/:id', usuariosPut);
 
-router.post('/', usuariosPost );
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es valido').custom(emailExiste).isEmail(),
+    check('rol').custom(esRoleValido),
+    validarCampos
+], usuariosPost);
 
-router.delete('/', usuariosDelete );
+router.delete('/', usuariosDelete);
 
-router.patch('/', usuariosPatch );
-
-
-
+router.patch('/', usuariosPatch);
 
 
 module.exports = router;
